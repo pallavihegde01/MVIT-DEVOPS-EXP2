244f57a91125a4bd7071529c1bee9a2100831334
Pallavi Hegde <pallavighegde16@gmail.com>
2025-04-22 15:08:03 +0530

    changed twice

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,12 @@
 function validateLogin(){
-    var userName = document.getElementById("username").value;
+    var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if(username === "admin" && password === "1234"){
         window.location.href = "home.html";
         return false;
-    }else{
-        alert("Invalid username or password");
+    }
+    else{
+        alert("Invalid username or password!");
         return false;
     }
 }
