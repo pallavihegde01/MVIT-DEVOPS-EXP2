30bdd63155070efbbbf5ced67e7278102e817c1c
Pallavi Hegde <pallavighegde16@gmail.com>
2025-04-22 14:33:02 +0530

    added docs for deployment

---
+++ docs/script.js
@@ -0,0 +1,11 @@
+function validateLogin(){
+    var userName = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username === "admin" && password === "1234"){
+        window.location.href = "home.html";
+        return false;
+    }else{
+        alert("Invalid username or password");
+        return false;
+    }
+}
